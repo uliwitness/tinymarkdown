92373e0bdcf28c0276c1e2194da8be45930e5a71
Uli Kusterer <ulivc@zathras.de>
2017-10-10 21:05:10 +0200

    Initial implementation. Parses *italics*, headlines underlined with === or --- and ![alt](url)-style image links. Also replaces line breaks and < and >.

--- tinymarkdown/main.cpp
+++ tinymarkdown/main.cpp
@@ -6,10 +6,25 @@
 //  Copyright © 2017 Uli Kusterer. All rights reserved.
 //
 
+#include "tinymarkdown.hpp"
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+int main( int argc, const char * argv[] )
+{
+	FILE* markdownFile = fopen( argv[1], "r" );
+	fseek( markdownFile, 0, SEEK_END );
+	long fileLength = ftell( markdownFile );
+	char* markdownStr = (char*) malloc( fileLength +1 );
+	fseek( markdownFile, 0, SEEK_SET );
+	fread( markdownStr, 1, fileLength, markdownFile );
+	fclose( markdownFile );
+	markdownStr[fileLength] = '\0';
+	
+	tinymarkdown::parser	parser;
+	
+	std::cout << parser.parse( markdownStr ) << std::endl;
+	
+	free(markdownStr);
+
 	return 0;
 }
---
+++ tinymarkdown/tinymarkdown.hpp
@@ -0,0 +1,215 @@
+//
+//  tinymarkdown.hpp
+//  tinymarkdown
+//
+//  Created by Uli Kusterer on 10.10.17.
+//  Copyright © 2017 Uli Kusterer. All rights reserved.
+//
+
+#ifndef tinymarkdown_hpp
+#define tinymarkdown_hpp
+
+#include <string>
+
+namespace tinymarkdown
+{
+	
+	const std::string lineBreak = "<br />\n";
+	
+	class parser
+	{
+	public:
+		std::string parse( std::string inMarkdown )
+		{
+			std::string	htmlResult;
+			std::string	currentLine;
+			std::string	previousLine;
+			size_t		markdownLength = inMarkdown.length();
+			char		previousChar = '\0';
+			int			underlineHeaderLevel = 0;
+			off_t		italicStartOffset = -1;
+			off_t		imageStartOffset = -1;
+
+			for( size_t x = 0; x < markdownLength; )
+			{
+				char currCh = inMarkdown[x];
+				
+				if( currCh == '\r' )	// Mac-style CR line ending, or maybe Windows-style CRLF.
+				{
+					if( underlineHeaderLevel > 0 )
+					{
+						char openingTag[20] = {};
+						snprintf( openingTag, sizeof(openingTag) -1, "<h%d>", underlineHeaderLevel );
+						currentLine = openingTag;
+						
+						size_t lineBreakOffset = previousLine.length() -lineBreak.length();
+						if( previousLine.rfind( lineBreak ) == lineBreakOffset )
+							previousLine.resize(lineBreakOffset);
+						currentLine.append( previousLine );
+						
+						char closingTag[20] = {};
+						snprintf( closingTag, sizeof(openingTag) -1, "</h%d>\n", underlineHeaderLevel );
+						currentLine.append( closingTag );
+
+						// Don't write out the previousLine, we incorporated it into our currentLine and would get it twice.
+					}
+					else
+					{
+						htmlResult.append( previousLine );
+						currentLine.append( lineBreak );
+					}
+					previousLine = currentLine;
+					currentLine = "";
+					previousChar = '\r';	// So next LF can be handled specially if this was a CRLF.
+					++x;
+					underlineHeaderLevel = 0;
+					italicStartOffset = -1;
+					continue;
+				}
+				
+				if( currCh == '\n' )
+				{
+					if( previousChar == '\r' )	// Windows-style CRLF sequence
+					{
+						// We already generated a BR tag for this line break in the CR code.
+						previousChar = '\n';
+					}
+					else	// Unix-style LF.
+					{
+						if( underlineHeaderLevel > 0 )
+						{
+							char openingTag[20] = {};
+							snprintf( openingTag, sizeof(openingTag) -1, "<h%d>", underlineHeaderLevel );
+							currentLine = openingTag;
+							
+							size_t lineBreakOffset = previousLine.length() -lineBreak.length();
+							if( previousLine.rfind( lineBreak ) == lineBreakOffset )
+								previousLine.resize(lineBreakOffset);
+							currentLine.append( previousLine );
+							
+							char closingTag[20] = {};
+							snprintf( closingTag, sizeof(openingTag) -1, "</h%d>\n", underlineHeaderLevel );
+							currentLine.append( closingTag );
+							
+							// Don't write out the previousLine, we incorporated it into our currentLine and would get it twice.
+						}
+						else
+						{
+							htmlResult.append( previousLine );
+							currentLine.append( lineBreak );
+						}
+						previousLine = currentLine;
+						currentLine = "";
+						underlineHeaderLevel = 0;
+					}
+					italicStartOffset = -1;
+					++x;
+					continue;
+				}
+				
+				if( currCh == '=' )	// Underlines previous line's text? Possibly start of headline!
+				{
+					currentLine.append("=");
+					if( underlineHeaderLevel == 0 )
+						underlineHeaderLevel = 1;
+					++x;
+				}
+				else if( currCh == '-' )	// Underlines previous line's text? Possibly start of headline!
+				{
+					currentLine.append("-");
+					if( underlineHeaderLevel == 0 )
+						underlineHeaderLevel = 2;
+					++x;
+				}
+				else if( currCh == '*' )
+				{
+					if( italicStartOffset > -1 )	// End of italic sequence.
+					{
+						currentLine.replace(italicStartOffset, 1, "<i>");
+						currentLine.append("</i>");
+						italicStartOffset = -1;
+					}
+					else
+					{
+						italicStartOffset = currentLine.length();
+						currentLine.append("*");
+					}
+					underlineHeaderLevel = -1;
+
+					++x;
+				}
+				else if( currCh == '<' )
+				{
+					currentLine.append( "&lt;" );
+					underlineHeaderLevel = -1;
+					++x;
+				}
+				else if( currCh == '>' )
+				{
+					currentLine.append( "&gt;" );
+					underlineHeaderLevel = -1;
+					++x;
+				}
+				else if( currCh == '!' )
+				{
+					imageStartOffset = currentLine.length();
+					currentLine.append( "!" );
+					underlineHeaderLevel = -1;
+					++x;
+				}
+				else if( currCh == ')' && imageStartOffset > -1 )
+				{
+					size_t imageInfoLength = currentLine.length() -imageStartOffset;
+					if( currentLine[imageStartOffset +1] == '[' )	// Valid alt-text! Image!
+					{
+						std::string imageInfo = currentLine.substr( imageStartOffset +2, imageInfoLength -2 );	// We know it started with "![", no need to extract that.
+						off_t	separatorOffset = imageInfo.find("](");
+						if( separatorOffset == std::string::npos )	// Not an image link.
+							currentLine.append( ")" );
+						else
+						{
+							std::string imageTag( "<img src=\"" );
+							imageTag.append( imageInfo.substr( separatorOffset +2, imageInfo.length() -separatorOffset -2 ) );
+							if( separatorOffset > 0 )
+							{
+								imageTag.append( "\" alt=\"" );
+								imageTag.append( imageInfo.substr( 0, separatorOffset ) );
+							}
+							imageTag.append( "\" />" );
+							currentLine.replace( imageStartOffset, imageInfoLength, imageTag );
+						}
+					}
+					else	// Not an image, leave untouched, and append the ")" we just saw.
+					{
+						currentLine.append( ")" );
+					}
+					underlineHeaderLevel = -1;
+					++x;
+				}
+				else
+				{
+					currentLine.append( 1, currCh );
+					underlineHeaderLevel = -1;
+					++x;
+				}
+
+				/* Previous iteration saw an asterisk and thought it might start italics
+					but there is whitespace after it? It's just an asterisk. */
+				if( italicStartOffset > -1 && italicStartOffset == (x -1)
+				   && (currCh == ' ' || currCh == '\t' || currCh == '\r' || currCh == '\n') )
+				{
+					italicStartOffset = -1;
+				}
+				
+				previousChar = currCh;
+			}
+			
+			htmlResult.append( previousLine );
+			htmlResult.append( currentLine );
+
+			return htmlResult;
+		}
+	};
+}
+
+#endif /* tinymarkdown_hpp */
