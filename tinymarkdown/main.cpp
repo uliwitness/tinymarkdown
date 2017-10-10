b2e5d5bf3d0796eb702d6176969be099d978a553
Uli Kusterer <ulivc@zathras.de>
2017-10-10 19:10:51 +0200

    Initial Commit

---
+++ tinymarkdown/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tinymarkdown
+//
+//  Created by Uli Kusterer on 10.10.17.
+//  Copyright © 2017 Uli Kusterer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
